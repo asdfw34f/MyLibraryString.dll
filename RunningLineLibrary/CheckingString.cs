d90254a4334535cb547dd9a031b07f8f8aa94bfb
asdfw34f <daniil2002.d11@gmail.com>
2022-11-17 12:57:33 +0300

    .dll

---
+++ RunningLineLibrary/CheckingString.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows.Forms;
+
+namespace MyStringLibrary
+{
+    internal class CheckingString
+    {
+        string SetRightCaps_RU;
+        string SetRight_RU;
+        string SetRightCaps_ENG;
+        string SetRight_ENG;
+        string SetDate;
+        string SetSpecial_Characters;
+
+        CheckingString()
+        {
+            SetRightCaps_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ";
+            SetRightCaps_ENG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+            SetRight_RU = SetRightCaps_RU.ToLower();
+            SetRight_ENG = SetRightCaps_ENG.ToLower();
+            SetDate = "1234567890.";
+            SetSpecial_Characters = @",.'"";:\|/{}[]_-+=~`!@#$%^&*()№<>";
+        }
+
+        protected internal bool CheckRightName(string text)
+        {
+            if (text == null)
+                return false;
+
+            foreach (int i in text)
+                foreach (int j in SetSpecial_Characters)
+                    if (text[i] == SetSpecial_Characters[j])
+                        return false;
+                    else if (text[i] != SetSpecial_Characters[j])
+                        continue;
+
+            return true;
+        }
+
+        protected internal string RedactRightName_RU(string text)
+        {
+            StringBuilder stringBuilder = new StringBuilder(text);
+            if (text == null)
+                return null;
+
+            if (!CheckRightName(text))
+            {
+                MessageBox.Show("Строка не прошла проверку: Специальные символы запрещены",
+                    "Ошибка ввода данных",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return null;
+            }
+
+            foreach (int i in text)
+                foreach (int j in SetSpecial_Characters)
+                    if (text[0] == SetRightCaps_RU[j])
+                        continue;
+                    else if (text[0] == SetRight_RU[j])
+                        stringBuilder[i] = SetRightCaps_RU[j];
+
+            return text;
+        }
+    }
+}
--- RunningLineLibrary/RunningLine.cs
+++ RunningLineLibrary/RunningLine.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows.Forms;
-using System.Threading;
 
-namespace RunningLineLibrary
+namespace MyStringLibrary
 {
     public class RunningLine
     {
@@ -16,38 +11,38 @@ namespace RunningLineLibrary
             text = null;
         }
 
-        public void RunLeft(TextBox textBox1)
+        protected internal void RunningLineLeft(TextBox textBox1, int ml_sec_SPEED)
         {
             for (int q = 0; q < 3; q++)
             {
                 string text = textBox1.Text;
                 int j = 0;
-                char temp;
+
                 while (j < text.Length)
                 {
-                    Thread.Sleep(95);
+                    Thread.Sleep(ml_sec_SPEED);
                     textBox1.Text = textBox1.Text.Remove(0, 1);
                     textBox1.Select(textBox1.TextLength - 1, 0);
-                    Thread.Sleep(95);
+                    Thread.Sleep(ml_sec_SPEED);
                     textBox1.Text = textBox1.Text.Insert(text.Length - 1, text.Substring(j, 1));
                     j++;
                 }
             }
         }
 
-        public void RunRight(TextBox textBox1)
+        protected internal void RunningLineRight(TextBox textBox1, int ml_sec_SPEED)
         {
             for (int q = 0; q < 3; q++)
             {
                 string text = textBox1.Text;
-                int j = text.Length -1;
-                char temp;
+                int j = text.Length - 1;
+
                 while (j >= 0)
                 {
-                    Thread.Sleep(95);
+                    Thread.Sleep(ml_sec_SPEED);
                     textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1, 1);
                     textBox1.Select(textBox1.TextLength - 1, 0);
-                    Thread.Sleep(95);
+                    Thread.Sleep(ml_sec_SPEED);
                     textBox1.Text = textBox1.Text.Insert(0, text.Substring(j, 1));
                     j--;
                 }
